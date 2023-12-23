f11e3acce4f4cb3d52487d820b08f723707965cb
unknown <abdulbatinchy@gmail.com>
2023-12-23 23:28:35 +0600

    g

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   RouterProvider
 } from "react-router-dom";
 import router from './Root.jsx';
 import MyContext from './Components/MyContext.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <MyContext>
     <RouterProvider router={router} />
     </MyContext>
+    </QueryClientProvider>
+    
+    
   </React.StrictMode>,
 )
