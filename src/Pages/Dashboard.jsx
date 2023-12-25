6e4de32aba88835b8e4a2bee427a2ff3a320dd34
unknown <abdulbatinchy@gmail.com>
2023-12-25 11:28:02 +0600

    delete added

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,11 +3,12 @@ import { MyContextProvider } from "../Components/MyContext";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useQuery } from "@tanstack/react-query";
+import deleteIcon from './../assets/delete.png'
 
 const Dashboard = () => {
     const {user}=useContext(MyContextProvider)
     const userEmail = user.email
-const handleSubmit =(e)=>{
+    const handleSubmit =(e)=>{
     e.preventDefault()
     const form = e.target;
     
@@ -31,7 +32,7 @@ const handleSubmit =(e)=>{
         showConfirmButton: false,
         timer: 1500,
       });
-    //   refetch()
+      refetch()
     }
   });
 }
@@ -46,8 +47,29 @@ const { data:tasks, refetch } = useQuery({
   });
 
 
-  const handleEdit=()=>{
+  const handleEdit=(id)=>{
+      console.log(id);
+      const editableObj = tasks.find(task =>task._id ==id)
+      console.log(editableObj);
 
+  }
+
+  const handleDelete = (_id)=>{
+    console.log(_id);
+    axios.delete(`http://localhost:5000/task/${_id}`)
+    .then((res) => {
+      console.log(res.data);
+      if (res.data.deletedCount) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "task deleted   successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        refetch()
+      }
+    });
   }
   
   return (
@@ -89,51 +111,31 @@ const { data:tasks, refetch } = useQuery({
                 </div>
                 {tasks?.map(task=>(
                 <div key={task._id} className="mb-5 bg-purple-400 px-4 py-2">
-                    <div className="flex justify-between items-center">
-                    <h3 className="text-white text-xl p font-medium my-4" >{task?.title}</h3>
-                    <button onClick={handleEdit}><img className="w-8 h-8" src="https://i.ibb.co/GkHfMr6/8666681-edit-icon.png" alt="" /></button>
-                    </div>
-                    <p className="text-white font-medium my-4">{task.desc}</p>
-                    <p className="text-white font-medium my-4">Time: {task.date}</p>
+                <div className="flex justify-between items-center">
+                  <h3 className="text-white text-xl p font-medium my-4" >{task?.title}</h3>
+                  <div>
+                  <button onClick={()=>handleEdit(task._id)}><img className="w-8 h-8" src="https://i.ibb.co/GkHfMr6/8666681-edit-icon.png" alt="" /></button>
+                  <button onClick={()=>handleDelete(task._id)}><img className="w-8 h-8" src={deleteIcon} alt="" /></button>
+                  </div>
+                </div>
+                <p className="text-white font-medium my-4">{task.desc}</p>
+                <p className="text-white font-medium my-4">Time: {task.date}</p>
                 </div>
                 ))}
             </div>
-            <div>
-            <div>
-                <div className="bg-blue-400 px-4 py-2 mt-16 mb-8">
-                    <h3 className="text-white font-medium ">Todo`s List</h3>
-                </div>
-                {tasks?.map(task=>(
-                <div key={task._id} className="mb-5 bg-purple-400 px-4 py-2">
-                    <div className="flex justify-between items-center">
-                    <h3 className="text-white text-xl p font-medium my-4" >{task?.title}</h3>
-                    <button onClick={handleEdit}><img className="w-8 h-8" src="https://i.ibb.co/GkHfMr6/8666681-edit-icon.png" alt="" /></button>
-                    </div>
-                    <p className="text-white font-medium my-4">{task.desc}</p>
-                    <p className="text-white font-medium my-4">Time: {task.date}</p>
-                </div>
-                ))}
+           
             </div>
-            </div>
-            <div>
-            <div>
-                <div className="bg-blue-400 px-4 py-2 mt-16 mb-8">
-                    <h3 className="text-white font-medium ">Todo`s List</h3>
-                </div>
-                {tasks?.map(task=>(
-                <div key={task._id} className="mb-5 bg-purple-400 px-4 py-2">
-                    <div className="flex justify-between items-center">
-                    <h3 className="text-white text-xl p font-medium my-4" >{task?.title}</h3>
-                    <button onClick={handleEdit}><img className="w-8 h-8" src="https://i.ibb.co/GkHfMr6/8666681-edit-icon.png" alt="" /></button>
-                    </div>
-                    <p className="text-white font-medium my-4">{task.desc}</p>
-                    <p className="text-white font-medium my-4">Time: {task.date}</p>
-                </div>
-                ))}
-            </div>
-            </div>
-        </div>
       </div>
+      <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 };
